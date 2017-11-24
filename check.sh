9e5c9d4f71f49f8604b197e7fca721561b2c1755
MatsunoM <matsunom@MatsunoMPro.local>
2017-11-24 23:06:57 +0900

    add basic 6 functions

--- check.sh
+++ check.sh
@@ -5,6 +5,13 @@
 dirpath=$HOME"/check"
 filepath=$HOME"/check/CHECK_TIME"
 
+check_nonactive() {
+  if [ "$1" = "" ] ; then #$1が空文字列だと終了する
+    echo "check requires function name!"
+    exit
+  fi
+}
+
 check_database() {
   if [ ! -e $dirpath ] ; then
     mkdir $dirpath
@@ -32,6 +39,7 @@ check_status() {
     elif [ $STATUS = 0 ] ; then 
       return 0
     else
+      sqlite3 CHECK_TIME "SELECT NAME FROM projects WHERE START = 1"
       exit
     fi
 }
@@ -54,11 +62,10 @@ create_project() {
 
 
 #チェックインする
-stamp_in() {
+check_in() {
   check_status
   check_status_return=$?
   if [ $check_status_return = 0 ] ; then #実行状態check_status_return=0だった場合、最後にOUTされているので、INできる
-    
     if [ -z "$2" ] ; then #projectが空文字列だと終了する
     echo "Please write a project name!"
     exit
@@ -95,12 +102,12 @@ substraction_time() {
   hours=`expr $get_time / 3600`
   minute=`expr \( $get_time % 3600 \) / 60`
   second=`expr $get_time % 60`
-  echo "Your check-out $NAME_STATUS is successful. This time is $hours:$minute:$second."
+  # echo "Your check-out $NAME_STATUS is successful. This time is $hours:$minute:$second."
   return 0
 }
 
 #チェックアウトする。
-stamp_out() {
+check_out() {
   check_status
   check_status_return=$?
   if [ $check_status_return = 1 ] ; then #実行状態check_status_return=1だった場合、最後はINなのでOUTする
@@ -111,12 +118,79 @@ stamp_out() {
     sqlite3 CHECK_TIME "UPDATE $NAME_STATUS SET END = '$DATE' WHERE ID = '$query_id'"
     sqlite3 CHECK_TIME "UPDATE projects SET START = 0 WHERE NAME = '$NAME_STATUS'"
     substraction_time  $NAME_STATUS $query_id
+    echo "Your check-out $NAME_STATUS is successful. This time is $hours:$minute:$second."
 
   elif [ $check_status_return = 0 ] ; then #実行状態check_status_return=1だった場合、最後はOUTなのでOUTできない
     echo "No projects is running!"
     exit
   fi
   return 0
+}
+
+check_list() {
+  sqlite3 CHECK_TIME "SELECT NAME FROM projects"
+}
+
+check_drop() {
+  check_project $1 $2
+  check_project_return=$?
+  if [ $check_project_return = 0 ] ; then #check_project_return=0だった場合、プロジェクトはは存在するのでプロジェクトの削除を行う。
+    sqlite3 CHECK_TIME "DROP TABLE '$2'"
+    sqlite3 CHECK_TIME "DELETE FROM projects WHERE NAME = '$2'"
+    echo "$2 is dropped."
+  else
+    echo "$2 is not exist."
+    exit
+  fi
+}
+
+check_delete() {
+  check_project $1 $2
+  check_project_return=$?
+  if [ $check_project_return = 0 ] ; then #check_project_return=0だった場合、プロジェクトはは存在するのでプロジェクトの削除を行う。
+    sqlite3 CHECK_TIME "SELECT * from '$2' WHERE ID = (SELECT max(ID) FROM '$2')"
+
+    echo "Do you delete this recort?(y/n)."
+    read delete_repry
+    if [ $delete_repry = 'y' ] ; then
+      sqlite3 CHECK_TIME "DELETE FROM '$2' WHERE ID = (SELECT max(ID) FROM '$2')"
+      echo "the record is deleted."
+      sqlite3 CHECK_TIME "UPDATE projects SET START = 0 WHERE NAME = '$2'"
+    else
+      echo "deleting is failed."
+      exit
+    fi
+
+  else
+    echo "$2 is not exist."
+    exit
+  fi
+}
+
+check_dump() {
+  echo ".dump" | sqlite3 CHECK_TIME > CHECK_TIME.dump
+  echo "creating dump file!"
+}
+
+check_restore() {
+    sqlite3 CHECK_TIME < CHECK_TIME.dump
+    echo "reading CHECK_TIME.dump to CHECK_TIME"
+}
+
+check_man() {
+  echo ""
+  echo "  check is a easy time logger tool."
+  echo ""
+  echo "    check has some functions."
+  echo "      check in [project] : You can start logging time."
+  echo "      check out : You can stop time logging."
+  echo "      check list : You can see a list of all projects."
+  echo "      check drop [project] : You can drop a project."
+  echo "      check delete [project] : You can delete a latest record from a project."
+  echo "      check dump : You can make a dumpfile."
+  echo "      check restore : You can restore from a dumpfile."
+  echo "      check man : You can see this command mannual."
+  echo ""
 }
 
 #スクリプトの流れ
@@ -127,6 +201,8 @@ stamp_out() {
 ###チェックアウト
 ####時間計算
 main() {
+  #$1がないなら終了する
+  check_nonactive $1
   #コマンドの実行環境の確認
   check_database
 
@@ -134,12 +210,28 @@ main() {
   cd $dirpath
   #実行状態の確認
   if [ $1 = "in" ] ; then
-    stamp_in $1 $2
-
+    check_in  $1 $2
 
   elif [ $1 = "out" ] ; then
-    # stamp_out $1 $2
-    stamp_out
+    check_out
+
+  elif [ $1 = "list" ] ; then
+    check_list
+
+  elif [ $1 = "drop" ] ; then
+    check_drop $1 $2
+
+  elif [ $1 = "delete" ] ; then
+    check_delete $1 $2
+
+  elif [ $1 = "dump" ] ; then
+    check_dump
+
+  elif [ $1 = "restore" ] ; then
+    check_dump
+
+  elif [ $1 = "man" ] ; then
+    check_man
   fi
 
   #元のディレクトリに移動する
